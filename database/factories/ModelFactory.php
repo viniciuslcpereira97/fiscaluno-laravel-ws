0ee6b2d2c5014bacc208950d692fae1afa8d907e
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-07-27 16:27:10 -0300

    made factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,56 @@ $factory->define(Fiscaluno\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(\Fiscaluno\Student\Student::class, function (Faker\Generator $faker) {
+    return [
+        'age' => $faker->numberBetween($min = 18, $max = 30),
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'gender' => $faker->name($gender =null | 'male' | 'female'),
+        'nacionality' => $faker->text,
+        'civilStatus' => $faker->text,
+        'address' => $faker->address,
+        'city' => $faker->city,
+        'state' => $faker->state,
+        'phone' => $faker->phoneNumber,
+        'institution_id' => 1
+    ];
+});
+
+$factory->define(\Fiscaluno\Institution\Institution::class, function(Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'address' => $faker->address,
+        'cnpj' => $faker->numberBetween($min = 100000000, $max = 999999999),
+        'email' => $faker->unique()->safeEmail,
+        'website' => $faker->domainName,
+        'phoneNumber' => $faker->phoneNumber,
+        'imageUri' => $faker->url
+    ];
+})
+
+$factory->define(\Fiscaluno\Review\GeneralReview::class, function(Faker\Generator $faker) {
+    return [
+        'course' => $faker->sentence($nbWords = 1, $variableNbWords = true),
+        'institution_id' => 1,
+        'student_id' => 1,
+        'suggestion' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'rate' => $faker->numberBetween($min = 0, $max = 10),
+        'pros' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'cons' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'description' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'payment' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'startYear' => $faker->date($format = 'd-m-Y', $max = 'now')
+    ];
+})
+
+$factory->define(\Fiscaluno\Review\DetailedReview::class, function(Faker\Generator $faker) {
+    return [
+       'course' => $faker->$faker->sentence($nbWords = 1, $variableNbWords = true),
+        'institution_id' => 1,
+        'student_id' => 1,
+        'type' => $faker->$faker->sentence($nbWords = 1, $variableNbWords = true),
+        'value' => $faker->numberBetween($min = 0, $max = 10)
+    ];
+})
