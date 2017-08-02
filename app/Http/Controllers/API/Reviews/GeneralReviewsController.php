7b413cf72f8dca753f3a77d4d20902d8d7e48c0e
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-02 17:58:46 -0300

    API controllers path created

---
+++ app/Http/Controllers/API/Reviews/GeneralReviewsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Fiscaluno\Http\Controllers\API\Reviews;
+
+use Illuminate\Http\Request;
+use Fiscaluno\Http\Controllers\Controller;
+
+class GeneralReviewsController extends Controller
+{
+    //
+}
