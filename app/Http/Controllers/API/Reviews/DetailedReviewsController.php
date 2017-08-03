e7499373428f60d7b9697e8a09d8c06d0b4616ca
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-03 10:49:40 -0300

    detailed reviews controller created

--- app/Http/Controllers/API/Reviews/DetailedReviewsController.php
+++ app/Http/Controllers/API/Reviews/DetailedReviewsController.php
@@ -5,7 +5,47 @@ namespace Fiscaluno\Http\Controllers\API\Reviews;
 use Illuminate\Http\Request;
 use Fiscaluno\Http\Controllers\Controller;
 
+use Fiscaluno\Repositories\Reviews\DetailedReviewsRepository;
+
 class DetailedReviewsController extends Controller
 {
-    //
+
+    /**
+     * Dependency Injection
+     * @param GeneralReviewsRepository $repository [description]
+     */
+    public function __construct(DetailedReviewsRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * All General Reviews
+     * @return Collection
+     */
+    public function allReviews()
+    {
+        return $this->repository->all();
+    }
+
+    /**
+     * General Review by Id
+     * @param  [type] $review_id [description]
+     * @return [type]            [description]
+     */
+    public function reviewById($review_id)
+    {
+        return $this->repository->byId($review_id);
+    }
+
+    /**
+     * General Reviews by institution
+     * @param  [type] $institutionId [description]
+     * @return [type]                [description]
+     */
+    public function reviewsByInstitution($institutionId)
+    {
+        return $this->repository->byInstitution($institutionId);
+    }
+
 }
