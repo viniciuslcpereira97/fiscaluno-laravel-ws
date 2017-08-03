7e31885e5f0e2c9a82134b071a84f3803f13f09b
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-03 16:18:25 -0300

    unit testing started

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+
     /**
      * A basic test example.
      *
@@ -19,5 +20,22 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * Tests all apis routes
+     * @return [type] [description]
+     */
+    public function testAPIReviewsRoutes()
+    {
+        foreach(\Route::getRoutes() as $route)
+        {
+            if($route->action["middleware"] === "api")
+            {
+                $uri = preg_replace('/\{.*\}/', "1", $route->uri);
+                $response = $this->get($uri);
+                $response->assertStatus(200);
+            }
+        }
     }
 }
