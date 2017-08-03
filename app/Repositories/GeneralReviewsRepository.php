ffd95c8d3dde91f9763ef1149fcc486f058df179
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-03 10:19:23 -0300

    Dependency injection at class construction

--- app/Repositories/GeneralReviewsRepository.php
+++ app/Repositories/GeneralReviewsRepository.php
@@ -2,7 +2,18 @@
 
 namespace Fiscaluno\Repositories;
 
+use Fiscaluno\Review\GeneralReview;
+
 class GeneralReviewsRepository extends BaseRepository
 {
+
+    /**
+     * Construct General Review model
+     * @param GeneralReview $reviews
+     */
+    public function __construct(GeneralReview $reviews)
+    {
+        $this->model = $reviews;
+    }
 
 }
