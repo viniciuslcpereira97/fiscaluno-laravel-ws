dc4c871e8f644b3e3406f0049668682e00c4b383
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-04 10:11:17 -0300

    api routes segmented

---
+++ routes/API/reviews.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Api Routes
+|--------------------------------------------------------------------------
+|
+| Here is located all reviews api routes
+|
+*/
+
+Route::group([
+    'prefix' => 'reviews',
+    'namespace' => 'API\Reviews',
+], function() {
+
+    $review_routes = function(){
+        Route::get('/', 'ReviewsController@allReviews');
+        Route::get('{review_id}', 'ReviewsController@reviewById');
+        Route::get('institution/{institution_id}', 'ReviewsController@reviewsByInstitution');
+        Route::get('author/{author_id}', 'ReviewsController@reviewsByAuthor');
+    };
+    
+    Route::group(['prefix'  =>  'general'], $review_routes);
+    Route::group(['prefix'  =>  'detailed'], $review_routes);
+});
--- routes/api.php
+++ routes/api.php
@@ -17,18 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group([
-    'prefix' => 'reviews',
-    'namespace' => 'API\Reviews',
-], function() {
-
-    $review_routes = function(){
-        Route::get('/', 'ReviewsController@allReviews');
-        Route::get('{review_id}', 'ReviewsController@reviewById');
-        Route::get('institution/{institution_id}', 'ReviewsController@reviewsByInstitution');
-        Route::get('author/{author_id}', 'ReviewsController@reviewsByAuthor');
-    };
-    
-    Route::group(['prefix'  =>  'general'], $review_routes);
-    Route::group(['prefix'  =>  'detailed'], $review_routes);
-});
+foreach(File::allFiles(__DIR__ . '/API') as $partial) {
+    require $partial->getPathname();
+}
