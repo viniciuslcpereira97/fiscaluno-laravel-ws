8bc6335123c7a1c660e8a11d7f2b3f29ae5b721a
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-14 12:21:54 -0300

    Reviews validation rules created but not implemented

---
+++ app/Http/Requests/ReviewRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Fiscaluno\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReviewRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'institution_id' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'institution_id:required' => 'opa fion'
+        ];
+    }
+
+}
